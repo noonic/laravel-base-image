9e17f8a0e8f4def300e2eb07fa455dd14f197da2
atulmy <atul.12788@gmail.com>
2015-12-14 16:37:05 +0530

    helper

--- src/ImageHelper.php
+++ src/ImageHelper.php
@@ -15,7 +15,7 @@ class ImageHelper
         return preg_replace('/^(.*)\.' . $extension . '$/', '$1_'.$size.'.' . $extension, $filename);
     }
 
-    public static function getImageUrl($filename, $size = '')
+    public static function getImageUrl($filename, $size = '', $default = '')
     {
         if(filter_var($filename, FILTER_VALIDATE_URL)) {
             return $filename;
@@ -25,16 +25,22 @@ class ImageHelper
 
         if($filename != '' && strlen($filename) !== 0) {
             if($size !== '') {
-                if(File::exists(public_path('images/uploads' . getImage($filename, $size)))) {
-                    $image = asset('images/uploads' . getImage($filename, $size));
+                if(File::exists(public_path('images/uploads' . self::getImage($filename, $size)))) {
+                    $image = asset('images/uploads' . self::getImage($filename, $size));
                 } else if(File::exists(public_path('images/uploads' . $filename))) {
-                    $image = asset('images/uploads' . $filename);
+                    $image = asset('images/uploads'.$filename);
+                } else {
+                    $image = asset('images/uploads'.self::getImage($default, $size));
                 }
             } else {
                 if(File::exists(public_path('/images/uploads'.$filename))) {
                     $image = asset('images/uploads'.$filename);
+                } else {
+                    $image = asset('images/uploads'.self::getImage($default, $size));
                 }
             }
+        } else {
+            $image = asset('images/uploads'.self::getImage($default, $size));
         }
         return $image;
     }
